2493991b165101d182639ce0aea28ffaa82e8038
rafeedm <rafeedmohtasim@gmail.com>
2021-03-15 05:12:50 -0400

    first push

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "Simple Fiqh",
+    description: "A reference for everyday fiqh",
+    themeConfig: {
+        logo: "/fiqh-temp-logo.png",
+        sidebar: [
+            '/'
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /fiqh-temp-logo.png
+heroTitle: Simple Fiqh
+tagline: Your reference for everyday fiqh
+---
+
