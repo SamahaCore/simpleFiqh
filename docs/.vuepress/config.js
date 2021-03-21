9e5a7c160ac830b13185b1c96ed4b2df74f40d33
rafeed <rafeedmohtasim@gmail.com>
2021-03-20 20:50:10 -0400

    added all salaah topics + about us link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,13 +11,15 @@ module.exports = {
                     { text: 'Hanafi', link: '/hanafi/' },
                     { text: 'Shafii', link: '/shafii/' }
                 ]
+            },
+            {
+                text: 'About', link: '/about'
             }
-
         ],
         sidebar: {
             '/hanafi/': [
                 {
-                    title: 'Purification (Tahaara)',   // required
+                    title: 'Purification',   // required
                     //path: '/foo/',      // optional, link of the title, which should be an absolute path and must exist
                     collapsable: false, // optional, defaults to true
                     sidebarDepth: 1,    // optional, defaults to 1
@@ -29,11 +31,32 @@ module.exports = {
                         '/hanafi/tahaara/najasa',
                         '/hanafi/tahaara/istinja',
                     ]
+                },
+                {
+                    title: 'Salaah',   // required
+                    //path: '/foo/',      // optional, link of the title, which should be an absolute path and must exist
+                    collapsable: false, // optional, defaults to true
+                    sidebarDepth: 1,    // optional, defaults to 1
+                    children: [
+                        '/hanafi/salaah/timings',
+                        '/hanafi/salaah/types',
+                        '/hanafi/salaah/adhan',
+                        '/hanafi/salaah/shara\'it',
+                        '/hanafi/salaah/arkaan',
+                        '/hanafi/salaah/wajibaat',
+                        '/hanafi/salaah/sunan',
+                        '/hanafi/salaah/adab',
+                        '/hanafi/salaah/mufsidat',
+                        '/hanafi/salaah/makruhaat',
+                        '/hanafi/salaah/travelling',
+                        '/hanafi/salaah/illness',
+                        '/hanafi/salaah/sujud',
+                    ]
                 }
             ],
             '/shafii/': [
                 {
-                    title: 'Purification (Tahaara)',   // required
+                    title: 'Purification',   // required
                     //path: '/foo/',      // optional, link of the title, which should be an absolute path and must exist
                     collapsable: false, // optional, defaults to true
                     sidebarDepth: 1,    // optional, defaults to 1
